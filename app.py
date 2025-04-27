f899e6d44c92da0b0008444a13363d121e9f45f1
Arymamburrem <construrapteam@gmail.com>
2025-04-27 15:03:44 -0300

    app.py

--- app.py
+++ app.py
@@ -100,5 +100,6 @@ y = data['Carreras Ganadas']
 # --- Entrenamiento del Modelo ---
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestRegressor(n_estimators=100, random_
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+
 
